e272ee4ee2b97c3e7812d562a86f6a6dfa9fc217
Lara Vettorazzi <lv82598@student.uni-lj.si>
2024-07-31 18:58:05 +0200

    napisali funkcijo, ki iz podatkov izlušči posamezen recept

--- izlusci_podatke.py
+++ izlusci_podatke.py
@@ -1,8 +1,16 @@
 import requests
+import re
 
 def prenesi_podatke(link):
     html = requests.get(link)
-    with open("html.txt", "w", encoding="utf8") as dat:
-        print(html.text, file=dat)
+    # with open("html.txt", "w", encoding="utf8") as dat:
+    #     print(html.text, file=dat)
+    return html.text
 
-prenesi_podatke("https://okusno.je/iskanje")
+# prenesi_podatke("https://okusno.je/iskanje")
+
+def najdi_recept(html: str):
+    vzorec = r'<span></span></div></div></a><a href=/recept/.*?<span></span>'
+    return re.findall(vzorec, html, flags=re.DOTALL)
+
+print(len(najdi_recept(prenesi_podatke("https://okusno.je/iskanje"))))
