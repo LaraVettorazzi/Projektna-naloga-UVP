8c23140aabadac2b2188a4b1a6bc2a483d116720
Lara Vettorazzi <lv82598@student.uni-lj.si>
2024-08-13 19:40:40 +0200

    importali smo funkcijo s pravim imenom in pognali funckijo naredi_csv

--- naredi_csv.py
+++ naredi_csv.py
@@ -1,5 +1,5 @@
 import csv
-from izlusci_podatke import prenesi_podatke, najdi_recept, najdi_url, najdi_vsebino
+from izlusci_podatke import prenesi_podatke, najdi_recept, najdi_url, izlusci_podatke
 
 def naredi_csv():
     podatki = prenesi_podatke('https://okusno.je/iskanje')
@@ -14,14 +14,14 @@ def naredi_csv():
         for recept in recepti:
             url = najdi_url(recept)
             vsebina_recept = prenesi_podatke('https://okusno.je' + url)
-            podatki_recept = najdi_vsebino(vsebina_recept)
+            podatki_recept = izlusci_podatke(vsebina_recept)
             pisatelj.writerow(
                 [
                     podatki_recept['ime']
                 ]
             )
 
-        
+naredi_csv()        
 
 
 
