12fceb5633869f7a4b1917c4f4ee14ea74b36f91
Lara Vettorazzi <lv82598@student.uni-lj.si>
2024-08-13 19:48:31 +0200

    pripravili smo si, kako bomo prenesli podatke iz več strani

--- naredi_csv.py
+++ naredi_csv.py
@@ -2,24 +2,25 @@ import csv
 from izlusci_podatke import prenesi_podatke, najdi_recept, najdi_url, izlusci_podatke
 
 def naredi_csv():
-    podatki = prenesi_podatke('https://okusno.je/iskanje')
-    recepti = najdi_recept(podatki)
-    with open('recepti.csv', 'w', encoding='utf8') as dat:
-        pisatelj = csv.writer(dat)
-        pisatelj.writerow(
-            [
-                'Ime'
-            ]
-        )
-        for recept in recepti:
-            url = najdi_url(recept)
-            vsebina_recept = prenesi_podatke('https://okusno.je' + url)
-            podatki_recept = izlusci_podatke(vsebina_recept)
+    for stran in range(1, vse_strani + 1):
+        podatki = prenesi_podatke(f'https://okusno.je/iskanje?t=recipe&sort=score&p={stran}')
+        recepti = najdi_recept(podatki)
+        with open('recepti.csv', 'w', encoding='utf8') as dat:
+            pisatelj = csv.writer(dat)
             pisatelj.writerow(
                 [
-                    podatki_recept['ime']
+                    'Ime'
                 ]
             )
+            for recept in recepti:
+                url = najdi_url(recept)
+                vsebina_recept = prenesi_podatke('https://okusno.je' + url)
+                podatki_recept = izlusci_podatke(vsebina_recept)
+                pisatelj.writerow(
+                    [
+                        podatki_recept['ime']
+                    ]
+                )
 
 naredi_csv()        
 
