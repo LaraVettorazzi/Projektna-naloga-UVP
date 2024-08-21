6bd95c396cdc68b0fb45ebecd3b82c1d877aec5c
Lara Vettorazzi <lv82598@student.uni-lj.si>
2024-08-21 16:31:35 +0200

    popravili smo funkcijo, saj je prej vsakič sproti zbrisala in v datoteko na novo napisala vse recepte

--- naredi_csv.py
+++ naredi_csv.py
@@ -1,24 +1,26 @@
 import csv
-from izlusci_podatke import prenesi_podatke, najdi_recept, najdi_url, izlusci_podatke
+from izlusci_podatke import prenesi_podatke, najdi_recept, najdi_url, izlusci_podatke, vse_strani
 
 def naredi_csv():
-    for stran in range(1, vse_strani + 1):
-        podatki = prenesi_podatke(f'https://okusno.je/iskanje?t=recipe&sort=score&p={stran}')
-        recepti = najdi_recept(podatki)
-        with open('recepti.csv', 'w', encoding='utf8') as dat:
-            pisatelj = csv.writer(dat)
-            pisatelj.writerow(
-                [
-                    'Ime'
-                ]
-            )
+    with open('recepti.csv', 'w', encoding='utf8') as dat:
+        pisatelj = csv.writer(dat)
+        pisatelj.writerow(
+            [
+                'Ime',
+                'Kategorija'
+            ]
+        )
+        for stran in range(1, 4): #popravi na vse_strani() + 1
+            podatki = prenesi_podatke(f'https://okusno.je/iskanje?t=recipe&sort=score&p={stran}')
+            recepti = najdi_recept(podatki)
             for recept in recepti:
                 url = najdi_url(recept)
                 vsebina_recept = prenesi_podatke('https://okusno.je' + url)
                 podatki_recept = izlusci_podatke(vsebina_recept)
                 pisatelj.writerow(
                     [
-                        podatki_recept['ime']
+                        podatki_recept['ime'],
+                        podatki_recept['kategorija']
                     ]
                 )
 
