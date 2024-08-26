ddc33bb6c515a3f7b049defbfa649538aee371d7
LaraVettorazzi <lv82598@student.uni-lj.si>
2024-08-26 19:28:27 +0200

    uporabili extension ruff, ki je polepšal kodo

--- izlusci_podatke.py
+++ izlusci_podatke.py
@@ -1,21 +1,22 @@
 import requests
 import re
 
+
 def prenesi_podatke(link):
     html = requests.get(link)
     return html.text
 
+
 def najdi_recept(html: str):
-    vzorec = r'<a href=/recept/.*?<span></span>'
+    vzorec = r"<a href=/recept/.*?<span></span>"
     return re.findall(vzorec, html, re.DOTALL)
 
+
 def najdi_url(recept: str):
-    vzorec = re.compile(
-        r'<a href=(?P<url>.+?) class=',
-        re.DOTALL
-    )
+    vzorec = re.compile(r"<a href=(?P<url>.+?) class=", re.DOTALL)
     najdba = vzorec.search(recept)
     return najdba["url"]
+
 
 def izlusci_podatke(recept: str):
     vzorec = re.compile(
@@ -24,86 +25,91 @@ def izlusci_podatke(recept: str):
         r'"recipeCategory":"(?P<kategorija>.+?)".*?'
         r'"recipeIngredient":\[(?P<sestavine>.+?)],.*?'
         r'"recipeInstructions":(?P<koraki>.*?)"nutrition".*?'
-        r'difficulty-(?P<tezavnost>\d).*?'
-        r'SKUPAJ</span>\n(?P<cas>.*?)</div></div></div>.*?'
-        r'<tr><td><td>(?P<hranilna_vrednost>.*?)<td>',
-        re.DOTALL
+        r"difficulty-(?P<tezavnost>\d).*?"
+        r"SKUPAJ</span>\n(?P<cas>.*?)</div></div></div>.*?"
+        r"<tr><td><td>(?P<hranilna_vrednost>.*?)<td>",
+        re.DOTALL,
     )
-    
+
     najdba = vzorec.search(recept)
     if najdba:
         slovar = {}
-        slovar['ime'] = najdba['ime']
-        slovar['kategorija'] = najdba['kategorija']
-        slovar['tezavnost'] = najdba['tezavnost']
-        slovar['cas'] = cas_v_minutah(najdba['cas'])
-        slovar['hranilna_vrednost'] = hranilna_vrednost_v_kcal(najdba['hranilna_vrednost'])
-        slovar['koraki'] = prestej_st_korakov(najdba['koraki'])
-        slovar['sestavine'] = prestej_sestavine(najdba['sestavine'])
+        slovar["ime"] = najdba["ime"]
+        slovar["kategorija"] = najdba["kategorija"]
+        slovar["tezavnost"] = najdba["tezavnost"]
+        slovar["cas"] = cas_v_minutah(najdba["cas"])
+        slovar["hranilna_vrednost"] = hranilna_vrednost_v_kcal(
+            najdba["hranilna_vrednost"]
+        )
+        slovar["koraki"] = prestej_st_korakov(najdba["koraki"])
+        slovar["sestavine"] = prestej_sestavine(najdba["sestavine"])
 
-        opis = str(najdba['opis'])
-        opis_popravljen = (opis.replace('\\u0027', '\'')
-                           .replace('\\u0022', '\'')
-                           .replace('u0022', '\'')
-                           .strip('\"')
-                           .replace(r'\r', ' ')
-                           .replace(r'\n', ' ')
-                           .replace(r'\/', '/')
-                           .replace(r'\t', ' ')
-                           .strip(r' '))
-        while '  ' in opis_popravljen:
-            opis_popravljen = opis_popravljen.replace('  ', ' ')
-        opis_popravljen = opis_popravljen.replace(' ', '').strip(' ')
+        opis = str(najdba["opis"])
+        opis_popravljen = (
+            opis.replace("\\u0027", "'")
+            .replace("\\u0022", "'")
+            .replace("u0022", "'")
+            .strip('"')
+            .replace(r"\r", " ")
+            .replace(r"\n", " ")
+            .replace(r"\/", "/")
+            .replace(r"\t", " ")
+            .strip(r" ")
+        )
+        while "  " in opis_popravljen:
+            opis_popravljen = opis_popravljen.replace("  ", " ")
+        opis_popravljen = opis_popravljen.replace(" ", "").strip(" ")
 
-        slovar['opis'] = opis_popravljen
-        slovar['st_besed'] = prestej_besede(opis_popravljen)
-        slovar['st_crk'] = prestej_crke(opis_popravljen)
+        slovar["opis"] = opis_popravljen
+        slovar["st_besed"] = prestej_besede(opis_popravljen)
+        slovar["st_crk"] = prestej_crke(opis_popravljen)
         return slovar
     else:
         {}
 
+
 def vse_strani():
-    podatki = prenesi_podatke('https://okusno.je/iskanje')
+    podatki = prenesi_podatke("https://okusno.je/iskanje")
     vzorec = re.compile(
         r'<button class="button-pag dark:text-white" name=p value=(?P<st_strani>\d+?)>Konec',
-        re.DOTALL
+        re.DOTALL,
     )
     najdba = vzorec.search(podatki)
-    return int(najdba['st_strani'])
+    return int(najdba["st_strani"])
+
 
 def cas_v_minutah(niz):
-    if 'h' in niz:
-        vzorec = re.compile(
-            r'(?P<ure>\d+) h (?P<minute>\d+) min'
-        )
+    if "h" in niz:
+        vzorec = re.compile(r"(?P<ure>\d+) h (?P<minute>\d+) min")
         najdba = vzorec.search(niz)
-        return int(najdba['ure']) * 60 + int(najdba['minute'])
+        return int(najdba["ure"]) * 60 + int(najdba["minute"])
     else:
-        vzorec = re.compile(
-            r'(?P<minute>\d+) min'
-        )
+        vzorec = re.compile(r"(?P<minute>\d+) min")
         najdba = vzorec.search(niz)
-        return int(najdba['minute'])
+        return int(najdba["minute"])
+
 
 def hranilna_vrednost_v_kcal(niz):
-    vzorec = re.compile(
-            r'(?P<kcal>[\d\.]+) kCal'
-        )
+    vzorec = re.compile(r"(?P<kcal>[\d\.]+) kCal")
     najdba = vzorec.search(niz)
-    return int(round(float(najdba['kcal']), 0))
+    return int(round(float(najdba["kcal"]), 0))
+
 
 def prestej_st_korakov(niz):
     najdba = re.findall("HowToStep", niz)
     return len(najdba)
 
+
 def prestej_sestavine(niz):
     najdba = re.findall('"[\d\w].*?"', niz)
     return len(najdba)
 
-def prestej_besede(niz:str):
-    return niz.count(' ') + 1
 
-def prestej_crke(niz:str):
+def prestej_besede(niz: str):
+    return niz.count(" ") + 1
+
+
+def prestej_crke(niz: str):
     seznam_besed = niz.split()
     st = 0
     for beseda in seznam_besed:
--- naredi_csv.py
+++ naredi_csv.py
@@ -1,46 +1,56 @@
 import csv
-from izlusci_podatke import prenesi_podatke, najdi_recept, najdi_url, izlusci_podatke, vse_strani
+from izlusci_podatke import (
+    prenesi_podatke,
+    najdi_recept,
+    najdi_url,
+    izlusci_podatke,
+    vse_strani,
+)
+
 
 def naredi_csv():
-    with open('recepti.csv', 'w', encoding='utf8') as dat:
+    with open("recepti.csv", "w", encoding="utf8") as dat:
         pisatelj = csv.writer(dat)
         pisatelj.writerow(
             [
-                'Ime',
-                'Kategorija',
-                'Težavnost',
-                'Čas (min)',
-                'Hranilna vrednost (kcal/100g)',
-                'Koraki',
-                'Sestavine',
-                'Opis',
-                'Dolžina opisa (besede)',
-                'Dolžina opisa (črke)'
+                "Ime",
+                "Kategorija",
+                "Težavnost",
+                "Čas (min)",
+                "Hranilna vrednost (kcal/100g)",
+                "Koraki",
+                "Sestavine",
+                "Opis",
+                "Dolžina opisa (besede)",
+                "Dolžina opisa (črke)",
             ]
         )
         for stran in range(1, vse_strani() + 1):
-            podatki = prenesi_podatke(f'https://okusno.je/iskanje?t=recipe&sort=score&p={stran}')
+            podatki = prenesi_podatke(
+                f"https://okusno.je/iskanje?t=recipe&sort=score&p={stran}"
+            )
             recepti = najdi_recept(podatki)
             for recept in recepti:
                 url = najdi_url(recept)
-                vsebina_recept = prenesi_podatke('https://okusno.je' + url)
+                vsebina_recept = prenesi_podatke("https://okusno.je" + url)
                 podatki_recept = izlusci_podatke(vsebina_recept)
                 if podatki_recept:
                     pisatelj.writerow(
                         [
-                            podatki_recept['ime'],
-                            podatki_recept['kategorija'],
-                            podatki_recept['tezavnost'],
-                            podatki_recept['cas'],
-                            podatki_recept['hranilna_vrednost'],
-                            podatki_recept['koraki'],
-                            podatki_recept['sestavine'],
-                            podatki_recept['opis'],
-                            podatki_recept['st_besed'],
-                            podatki_recept['st_crk']
+                            podatki_recept["ime"],
+                            podatki_recept["kategorija"],
+                            podatki_recept["tezavnost"],
+                            podatki_recept["cas"],
+                            podatki_recept["hranilna_vrednost"],
+                            podatki_recept["koraki"],
+                            podatki_recept["sestavine"],
+                            podatki_recept["opis"],
+                            podatki_recept["st_besed"],
+                            podatki_recept["st_crk"],
                         ]
                     )
                 else:
                     continue
 
-naredi_csv()        
+
+naredi_csv()
